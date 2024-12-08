12e9ac7fdcae7a8dd352f37de01fff0d7f133c60
Christophe Peugnet <christophe.peugnet@outlook.fr>
2024-12-08 19:24:28 +0100

    Add project files.

---
+++ Blazor.WinOld.Components/Components/Base/WinOldComponentBase.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Blazor.WinOld.Components;
+
+public abstract class WinOldComponentBase : ComponentBase
+{
+    /// <summary>
+    /// Optional CSS class names. If given, these will be included in the class attribute of the component.
+    /// </summary>
+    [Parameter]
+    public virtual string? Class { get; set; } = null;
+
+    /// <summary>
+    /// Optional in-line styles. If given, these will be included in the style attribute of the component.
+    /// </summary>
+    [Parameter]
+    public virtual string? Style { get; set; } = null;
+}
---
+++ Blazor.WinOld.Components/Components/Button/WinOldButton.razor.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+
+namespace Blazor.WinOld.Components;
+
+public partial class WinOldButton : WinOldComponentBase
+{
+    /// <summary>
+    /// Disables the form control, ensuring it doesn't participate in form submission.
+    /// </summary>
+    [Parameter]
+    public bool Disabled { get; set; }
+
+    /// <summary>
+    /// Gets or sets the content to be rendered inside the component.
+    /// </summary>
+    [Parameter]
+    public RenderFragment? ChildContent { get; set; }
+
+    /// <summary>
+    /// Command executed when the user clicks on the button.
+    /// </summary>
+    [Parameter]
+    public EventCallback<MouseEventArgs> OnClick { get; set; }
+
+    protected async Task OnClickHandlerAsync(MouseEventArgs e)
+    {
+        if (!Disabled && OnClick.HasDelegate)
+        {
+            await OnClick.InvokeAsync(e);
+        }
+
+        await Task.CompletedTask;
+    }
+}
