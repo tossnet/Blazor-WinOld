ea87bab326e467715d5ae54a6a338f3b043894dc
Christophe Peugnet <christophe.peugnet@outlook.fr>
2024-12-09 11:17:37 +0100

    WIP chkbx

---
+++ Blazor.WinOld.Components/Components/CheckBox/WinOldCheckBox.razor.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+
+namespace Blazor.WinOld.Components;
+
+public partial class WinOldCheckBox : WinOldComponentBase
+{
+    /// <summary>
+    /// Disables the form control, ensuring it doesn't participate in form submission.
+    /// </summary>
+    [Parameter]
+    public bool Disabled { get; set; }
+
+
+    /// </summary>
+    [Parameter]
+    [Category(CategoryTypes.Button.Appearance)]
+    public Appearance Appearance { get; set; }
+
+
+    /// </summary>
+    [Parameter]
+    public bool Checked { get; set; } 
+
+    /// </summary>
+    [Parameter]
+    public string Label { get; set; } = string.Empty;
+
+    private Guid _elementId { get; set; } = Guid.NewGuid();
+}
