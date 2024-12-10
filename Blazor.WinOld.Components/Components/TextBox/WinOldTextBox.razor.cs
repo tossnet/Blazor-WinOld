e292c3bf1a29009d731275efb97bc0702a0b7180
Christophe Peugnet <christophe.peugnet@outlook.fr>
2024-12-10 09:26:24 +0100

    add textbox

---
+++ Blazor.WinOld.Components/Components/TextBox/WinOldTextBox.razor.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+
+namespace Blazor.WinOld.Components;
+
+public partial class WinOldTextBox : WinOldComponentBase
+{
+
+    /// <summary>
+    /// Disables the form control, ensuring it doesn't participate in form submission.
+    /// </summary>
+    [Parameter]
+    public bool Disabled { get; set; }
+
+    /// </summary>
+    [Parameter]
+    [Category(CategoryTypes.Button.Appearance)]
+    public Appearance Appearance { get; set; }
+
+    /// </summary>
+    [Parameter]
+    public string Label { get; set; } = string.Empty;
+
+
+    /// </summary>
+    private Guid ElementId { get; set; } = Guid.NewGuid();
+
+
+    /// </summary>
+    private string GetComponentClass()
+    {
+        return Appearance switch
+        {
+            Appearance.Win7 => "txt-win-7",
+            Appearance.WinXP => "txt-win-xp",
+            Appearance.Win98 => "txt-win-98",
+            _ => "txt-win-98"
+        };
+    }
+}
