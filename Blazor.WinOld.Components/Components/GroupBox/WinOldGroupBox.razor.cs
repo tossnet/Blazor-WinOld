0f843a32ce82010dd938ed5dc883bf8aa85771b4
Christophe Peugnet <christophe.peugnet@outlook.fr>
2024-12-12 15:30:08 +0100

    add GroupBox

---
+++ Blazor.WinOld.Components/Components/GroupBox/WinOldGroupBox.razor.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Blazor.WinOld.Components;
+
+public partial class WinOldGroupBox : WinOldComponentBase
+{
+    /// </summary>
+    [Parameter]
+    public RenderFragment ChildContent { get; set; }
+
+
+    /// </summary>
+    [Parameter]
+    [Category(CategoryTypes.Button.Appearance)]
+    public Appearance Appearance { get; set; }
+
+    /// </summary>
+    [Parameter]
+    public string Label { get; set; } = string.Empty;
+
+    /// </summary>
+    private string GetComponentClass()
+    {
+        return Appearance switch
+        {
+            Appearance.Win7 => "fieldset-win-7",
+            Appearance.WinXP => "fieldset-win-xp",
+            Appearance.Win98 => "fieldset-win-98",
+            _ => "fieldset-win-98"
+        };
+    }
+}
--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 # Blazor.WinOld
 
+[![NuGet](https://img.shields.io/nuget/v/BlazorWinOld.svg)](https://www.nuget.org/packages/BlazorWinOld/)  ![BlazorCalendar Nuget Package](https://img.shields.io/nuget/dt/BlazorCalendar)
+[![GitHub](https://img.shields.io/github/license/tossnet/Blazor-WinOld?color=594ae2&logo=github&style=flat-square)](https://github.com/tossnet/Blazor-WinOld/blob/main/LICENSE)
+
+For Blazor Server or Blazor WebAssembly
+
+
 ## Hello, world!
 Welcome to your new madness. I'm using Windows styles made in css by :
 - https://github.com/botoxparty/XP.css
