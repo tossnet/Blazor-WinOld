f87e256890037d24d47394e785135a6c6f1b4cc5
Christophe Peugnet <christophe.peugnet@outlook.fr>
2024-12-20 14:47:51 +0100

    v0.0.6 Add a new component : DropDown

---
+++ Blazor.WinOld.Components/Components/DropDown/WinOldOption.razor.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Blazor.WinOld.Components;
+
+public partial class WinOldOption<T> : WinOldComponentBase
+{
+    /// </summary>
+    [CascadingParameter]
+    public WinOldSelect<T?> Select { get; set; }
+
+    /// </summary>
+    [Parameter]
+    public RenderFragment? ChildContent { get; set; }
+
+    /// </summary>
+    [CascadingParameter(Name = "Appearance")]
+    public virtual Appearance Appearance { get; set; }
+
+    /// </summary>
+    [Parameter]
+    public T Value { get; set; }
+
+    private bool IsSelected => Select != null &&  EqualityComparer<T>.Default.Equals(Value, Select.Value);
+
+    protected override void OnInitialized()
+    {
+        Select?.AddOption(this);
+    }
+
+}
+
---
+++ Blazor.WinOld.Components/Components/DropDown/WinOldSelect.razor.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Components;
+
+namespace Blazor.WinOld.Components;
+
+public partial class WinOldSelect<T> : WinOldComponentBase
+{
+    /// </summary>
+    [Parameter]
+    public RenderFragment? ChildContent { get; set; }
+
+    /// </summary>
+    [Parameter]
+    [Category(CategoryTypes.Button.Appearance)]
+    public Appearance Appearance { get; set; }
+
+    /// </summary>
+    [Parameter]
+    public T? Value { get; set; } = default(T);
+
+    /// </summary>
+    [Parameter]
+    public EventCallback<T> ValueChanged { get; set; }
+
+    internal HashSet<WinOldOption<T>> Options { get; } = new();
+
+    internal void AddOption(WinOldOption<T> option)
+    {
+        if (!Options.Contains(option))
+        {
+            Options.Add(option);
+        }
+    }
+    private async Task OnChange(ChangeEventArgs e)
+    {
+        if (e.Value is not null && Options.FirstOrDefault(opt => opt.Value?.ToString() == e.Value.ToString()) is WinOldOption<T> selectedOption)
+        {
+            Value = selectedOption.Value;
+            await ValueChanged.InvokeAsync(Value);
+        }
+    }
+
+    /// </summary>
+    private string GetComponentClass()
+    {
+        return Appearance switch
+        {
+            Appearance.Win7 => "select-win-7",
+            Appearance.WinXP => "select-win-xp",
+            Appearance.Win98 => "select-win-98",
+            _ => "select-win-98"
+        };
+    }
+}
